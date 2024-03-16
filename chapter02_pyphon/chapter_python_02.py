528e8eca3c9b3612d37dc6aaa21034829e75b2ef
yuyuhi <zaien24@naver.com>
2024-03-16 21:23:18 +0900

    240316_파이썬 기초(2)

---
+++ chapter02_pyphon/chapter_python_02.py
@@ -0,0 +1,21 @@
+# if, for, while
+
+# true
+condition = 2 > 1
+print(condition)
+print(type(condition))
+
+if 2 > 1 :
+    print("here")
+    
+
+n = 2
+
+if n == 10:
+    print("top")        
+elif n == 5:    
+    print("middle")    
+else:
+    print("bottom")
+    
+guess = int(input("숫자를 입력하세요"))    
