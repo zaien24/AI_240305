09bcc85690953f502ba40d9ca2e0f30f86077099
yuyuhi <zaien24@naver.com>
2024-03-17 22:06:23 +0900

    ch04-02. chatgpt api 사용 방법_4

--- CHAPTER03_OPENAI/play_with_openai.py
+++ CHAPTER03_OPENAI/play_with_openai.py
@@ -4,30 +4,12 @@ import openai
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-query = input("입력하세요")
-
-prompt = """
-다음 문장이 긍정이면 positive, 부정이면 negative를 만들어라
-
-text: 이 영화 최악이다
-sentiment: negative
-
-text: 배우들이 연기를 너무 잘하네
-sentiment: positive
-
-text: """
-
-promt = prompt + query + "\nsentiment: "
-
-print(promt)
-
-response = openai.Completion.create(model="text-davinci-003",
-                                    prompt=prompt,
-                                    temperature=0,
-                                    max_tokens=7)
-
-
-print(response)
-print(response['choices'][0]['test'])
-
-
+reponse = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": "Who won the world series in 2020?"},
+                {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+                {"role": "user", "content": "Where was it played?"}            
+        ]
+)
