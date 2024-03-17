4d09086b5ae43ebb0593ec82891df21502efd5e0
yuyuhi <zaien24@naver.com>
2024-03-17 22:02:37 +0900

    ch04-02. chatgpt api 사용 방법_3

--- CHAPTER03_OPENAI/play_with_openai.py
+++ CHAPTER03_OPENAI/play_with_openai.py
@@ -4,6 +4,8 @@ import openai
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+query = input("입력하세요")
+
 prompt = """
 다음 문장이 긍정이면 positive, 부정이면 negative를 만들어라
 
@@ -13,8 +15,11 @@ sentiment: negative
 text: 배우들이 연기를 너무 잘하네
 sentiment: positive
 
-text: 다시 보고싶은 영화에요.
-sentiment:"""
+text: """
+
+promt = prompt + query + "\nsentiment: "
+
+print(promt)
 
 response = openai.Completion.create(model="text-davinci-003",
                                     prompt=prompt,
