0dabeeb582012efd2d78594eb996a7fad1357fc5
yuyuhi <zaien24@naver.com>
2024-03-19 12:46:45 +0900

    CH06-04. ChatGPT API로 요약 서비스 만들기_03

--- 06/06-04/app.py
+++ 06/06-04/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import pandas as pd
 
 summarize_url = "http://localhost:8000/summarize"
 
@@ -7,6 +8,16 @@ def summarize(text):
     response = requests.post(summarize_url, json={"text": text})
     summary = response.json()["summary"]
     return summary
+
+def summarize_df(df):
+    news_summaries : []
+    for news_origin in df['뉴스원문']:
+        summary = summarize(news_origin)
+        news_summaries.append(summary)
+    df['뉴스요약'] - news_summaries
+    
+    return df
+    
 
 st.title("요약 서비스")
 tab1, tab2 = st.tabs(["실시간", "파일 업로드"])
@@ -22,3 +33,23 @@ with tab1:
                 st.error("요청 오류가 발생했습니다")
         else:
             st.warning("텍스트를 입력하세요")
+
+with tab2:
+    uploaded_file = st.file_uploader("Choose a file")
+
+    if uploaded_file:
+        st.success("업로드 성공!")
+        
+        progress_bar = st.progress(0, text="progress")
+        
+        df = pd.read_excel(uploaded_file)
+        
+        st.dataframe(df)
+        
+        new_df = summarize_df(df)
+        
+        st.dataframe(new_df)
+        
+        
+
+
