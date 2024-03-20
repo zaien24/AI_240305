17fdedf40eaa7ba6e4ff3210e007f970552b69d1
yuyuhi <zaien24@naver.com>
2024-03-20 12:43:44 +0900

    CH06-04. ChatGPT API로 초간단 챗봇 만들기_02

--- 06/06-05/backend.py
+++ 06/06-05/backend.py
@@ -1,3 +1,5 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 import openai
 
 openai.api_key = "<YOUR_OPENAI_API_KEY>"
@@ -9,6 +11,16 @@ def chat(messages):
     assistant_turn = resp_dict['choices'][0]['message']
     return assistant_turn # {"role": "assistant", "content": "blahblahblah"}
 
-messages = [{"role": "user", "content": "너는 누구야"}]
+app = FastAPI()
 
-print(chat(messages=messages))
+class Turn(BaseModel):
+    role: str
+    content: str
+
+class Messages(BaseModel):
+    messages: List[Turn]  # [{"role": "user", "content": "blahblahblah"}, {"role": "assistant", "content": "blahblahblah"}, ...]
+    
+@app.post("/chat")
+def post_chat(messages: Messages):
+    assistant_turn = chat(messages=messages['messages'])
+    return assistant_turn
