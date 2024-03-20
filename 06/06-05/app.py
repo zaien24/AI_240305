89d8bc42959eaec21bf6db2079a1ea8b3e0feac5
yuyuhi <zaien24@naver.com>
2024-03-20 12:52:50 +0900

    CH06-04. ChatGPT API로 초간단 챗봇 만들기_05

--- 06/06-05/app.py
+++ 06/06-05/app.py
@@ -1,5 +1,19 @@
 import streamlit as st
 from streamlit_chat import message
 
-message("this is assistant")
-message("this is user", is_user=True)
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = []
+    # [{"role": "user", "content": "blahblahblah"},
+    #  {"role": "assistant", "content": "blahblahblah"}, ...]
+
+chat_url = "http://localhost:8000/chat"
+
+st.title("챗봇 서비스")
+
+for i, msg_obj in enumerate(st.session_state['messages']):
+        msg = msg_obj['content']
+        is_user = False
+        if i % 2 == 0:
+            is_user = True
+
+        message(msg, is_user=is_user)
